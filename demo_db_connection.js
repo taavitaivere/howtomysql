1e6f5d2c2ffd0d2f2bec2f37911052daec31ec12
Taavi taivere <taavi.taivere@khk.ee>
2022-02-02 23:07:26 +0200

    Drop Table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -9,9 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    var sql = "DROP TABLE IF EXISTS customers";
     con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Number of records deleted: " + result.affectedRows);
+        console.log(result);
     });
 });
