980912da3bd21d4c4f5fcbe40d55f305eb4c4ad7
Taavi taivere <taavi.taivere@khk.ee>
2022-02-02 22:52:54 +0200

    insert into table

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -3,14 +3,15 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "qwerty"
+    password: "qwerty",
+    database: "mydb"
 });
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE mydb", function (err, result) {
+    var sql = "INSERT INTO customers (name, address) VALUES ('Michelle', 'Blue Village 1')";
+    con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("Database created");
+        console.log("1 record inserted, ID: " + result.insertId);
     });
 });
